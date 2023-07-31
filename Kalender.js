f983f8d3a23fe84ad383c37bd1039897e6ee6cab
DomenicJaeger <140395824+DomenicJaeger@users.noreply.github.com>
2023-07-31 14:24:33 +0200

    Funktioniert Github

--- Kalender.js
+++ Kalender.js
@@ -1,6 +1,20 @@
-/*window.onload = function() {
-    alert("hello!");
-}*/
-window.onload="komisch()";
-function komisch(){
-alert("sehr seltsam");}
+let weekday=["Sonntag","Montag","Dienstag","Mittwoch","Donnerstag","Freitag","Samstag"];
+let monthName=["Januar","Februar","März","April","Mai","Juni","Juli","August","September","Oktober","November","Dezember"];
+let date=new Date();
+let dateB=date.toLocaleDateString("de-De");
+let year=new Date().getFullYear();
+let month=monthName[date.getMonth()];
+let day=weekday[date.getDay()];
+function getWeekOfMonth(date) {
+    let adjustedDate = date.getDate()+ date.getDay();
+    let prefixes = ['0', '1', '2', '3', '4', '5'];
+    return (parseInt(prefixes[0 | adjustedDate / 7])+1);
+}
+
+document.getElementById("infotext_year").innerHTML=year;
+document.getElementById("dateD").innerHTML=dateB;
+document.getElementById("weekdayD").innerHTML=day;
+document.getElementById("weekdayDa").innerHTML=day;
+document.getElementById("weekInMonth").innerHTML=(getWeekOfMonth(date));
+document.getElementById("monthID").innerHTML=month;
+//document.getElementById("holidayYesNoD").innerHTML=
