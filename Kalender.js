d776cdeac2522acf050ad919585ec49532af323d
Domenic Jaeger <domenic_jaeger@teilnehmer.cdemy.de>
2023-07-31 16:38:54 +0200

    header und historyhead mit date versehen

--- Kalender.js
+++ Kalender.js
@@ -1,11 +1,13 @@
 let weekday=["Sonntag","Montag","Dienstag","Mittwoch","Donnerstag","Freitag","Samstag"];
 let monthName=["Januar","Februar","März","April","Mai","Juni","Juli","August","September","Oktober","November","Dezember"];
 let date=new Date();
+let dateA=date.toLocaleDateString("de-De");
 let dateB=date.toLocaleDateString("de-De");
 let dateC=date.toLocaleDateString("de-De");
 let year=new Date().getFullYear();
 let month=monthName[date.getMonth()];
 let day=weekday[date.getDay()];
+let daylist=new Date().getDate();
 function getWeekOfMonth(date) {
     let adjustedDate = date.getDate()+ date.getDay();
     let prefixes = ['0', '1', '2', '3', '4', '5'];
@@ -13,10 +15,13 @@ function getWeekOfMonth(date) {
 }
 //document.querySelectorAll('p').innerHTML='hallo'
 document.getElementById("infotext_year").innerHTML=year;
-document.getElementById("dateDa").innerHTML=dateC;
-document.getElementsByClassName("dateD").innerHTML=dateB;
+document.getElementById("dateDa").innerHTML=dateA;
+document.getElementById("dateDb").innerHTML=dateB;
+document.getElementById("dateDc").innerHTML=dateC;
 document.getElementById("weekdayD").innerHTML=day;
 document.getElementById("weekdayDa").innerHTML=day;
 document.getElementById("weekInMonth").innerHTML=(getWeekOfMonth(date));
 document.getElementById("monthID").innerHTML=month;
+document.getElementById("lustig").innerHTML=daylist;
+document.getElementById("unlustig").innerHTML=month;
 //document.getElementById("holidayYesNoD").innerHTML=
